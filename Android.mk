17f9b8e287d6fc059779270f6a3e2a62b968c478
Joey Hewitt <joey@joeyhewitt.com>
2017-08-31 13:41:32 -0700

    android build: allow board to specify ril or gobi driver

--- Android.mk
+++ Android.mk
@@ -19,8 +19,6 @@ LOCAL_PATH := $(call my-dir)
 #   --prefix=/system \
 #   --localstatedir=/data/misc/radio/ofono
 
-# src/genbuiltin (make -f (printf 'printvars:\n\t @echo $(builtin_modules)' | psub) -f Makefile) > src/builtin.h
-
 include $(CLEAR_VARS)
 	LOCAL_MODULE := ofonod
 
@@ -32,25 +30,14 @@ include $(CLEAR_VARS)
 
 	# make -f (printf 'printvars:\n\t @echo $(src_ofonod_OBJECTS) $(gdbus_libgdbus_internal_la_OBJECTS)' | psub) -f Makefile | sed 's/\.l\?o/.c/g' | xargs -n 1 echo | sort | xargs echo | fmt | sed 's/$/ \\\\/'
 	LOCAL_SRC_FILES := \
-		drivers/rilmodem/call-barring.c drivers/rilmodem/call-forwarding.c \
-		drivers/rilmodem/call-settings.c drivers/rilmodem/call-volume.c \
-		drivers/rilmodem/cbs.c drivers/rilmodem/devinfo.c drivers/rilmodem/gprs.c \
-		drivers/rilmodem/gprs-context.c drivers/rilmodem/lte.c \
-		drivers/rilmodem/netmon.c drivers/rilmodem/network-registration.c \
-		drivers/rilmodem/radio-settings.c drivers/rilmodem/rilmodem.c \
-		drivers/rilmodem/rilutil.c drivers/rilmodem/sim.c \
-		drivers/rilmodem/sms.c drivers/rilmodem/stk.c drivers/rilmodem/ussd.c \
-		drivers/rilmodem/voicecall.c gatchat/crc-ccitt.c gatchat/gatchat.c \
-		gatchat/gathdlc.c gatchat/gatio.c gatchat/gatmux.c gatchat/gatppp.c \
-		gatchat/gatrawip.c gatchat/gatresult.c gatchat/gatserver.c \
-		gatchat/gatsyntax.c gatchat/gattty.c gatchat/gatutil.c \
-		gatchat/gsm0710.c gatchat/ppp_auth.c gatchat/ppp_cp.c \
-		gatchat/ppp_ipcp.c gatchat/ppp_ipv6cp.c gatchat/ppp_lcp.c \
+		gatchat/crc-ccitt.c gatchat/gatchat.c gatchat/gathdlc.c \
+		gatchat/gatio.c gatchat/gatmux.c gatchat/gatppp.c gatchat/gatrawip.c \
+		gatchat/gatresult.c gatchat/gatserver.c gatchat/gatsyntax.c \
+		gatchat/gattty.c gatchat/gatutil.c gatchat/gsm0710.c gatchat/ppp_auth.c \
+		gatchat/ppp_cp.c gatchat/ppp_ipcp.c gatchat/ppp_ipv6cp.c gatchat/ppp_lcp.c \
 		gatchat/ppp_net.c gatchat/ringbuffer.c gdbus/client.c gdbus/mainloop.c \
-		gdbus/object.c gdbus/polkit.c gdbus/watch.c gril/gril.c \
-		gril/grilio.c gril/grilutil.c gril/parcel.c plugins/allowed-apns.c \
-		plugins/infineon.c plugins/push-notification.c plugins/ril.c \
-		plugins/rildev.c plugins/ril_intel.c plugins/smart-messaging.c \
+		gdbus/object.c gdbus/polkit.c gdbus/watch.c plugins/allowed-apns.c \
+		plugins/push-notification.c plugins/smart-messaging.c \
 		src/audio-settings.c src/call-barring.c src/call-forwarding.c \
 		src/call-meter.c src/call-settings.c src/call-volume.c src/cbs.c \
 		src/cdma-connman.c src/cdma-netreg.c src/cdma-provision.c src/cdma-sms.c \
@@ -67,6 +54,34 @@ include $(CLEAR_VARS)
 		\
 		android/glibc.c android/log.c
 
+	OFONO_PLUGINS := smart_messaging push_notification allowed_apns
+
+	ifeq ($(BOARD_OFONO_DRIVER),ril)
+		LOCAL_SRC_FILES += \
+			drivers/rilmodem/call-barring.c drivers/rilmodem/call-forwarding.c \
+			drivers/rilmodem/call-settings.c drivers/rilmodem/call-volume.c \
+			drivers/rilmodem/cbs.c drivers/rilmodem/devinfo.c drivers/rilmodem/gprs.c \
+			drivers/rilmodem/gprs-context.c drivers/rilmodem/lte.c \
+			drivers/rilmodem/netmon.c drivers/rilmodem/network-registration.c \
+			drivers/rilmodem/radio-settings.c drivers/rilmodem/rilmodem.c \
+			drivers/rilmodem/rilutil.c drivers/rilmodem/sim.c \
+			drivers/rilmodem/sms.c drivers/rilmodem/stk.c drivers/rilmodem/ussd.c \
+			drivers/rilmodem/voicecall.c gril/gril.c gril/grilio.c gril/grilutil.c \
+			gril/parcel.c plugins/infineon.c plugins/ril.c plugins/rildev.c \
+			plugins/ril_intel.c
+		OFONO_PLUGINS += rildev ril infineon ril_intel rilmodem
+	else ifeq ($(BOARD_OFONO_DRIVER),gobi)
+		LOCAL_SRC_FILES += \
+			drivers/qmimodem/devinfo.c drivers/qmimodem/gprs.c \
+			drivers/qmimodem/gprs-context.c drivers/qmimodem/location-reporting.c \
+			drivers/qmimodem/nas.c drivers/qmimodem/network-registration.c \
+			drivers/qmimodem/qmi.c drivers/qmimodem/qmimodem.c \
+			drivers/qmimodem/radio-settings.c drivers/qmimodem/sim.c \
+			drivers/qmimodem/sim-legacy.c drivers/qmimodem/sms.c \
+			drivers/qmimodem/ussd.c drivers/qmimodem/voicecall.c plugins/gobi.c
+		OFONO_PLUGINS += gobi qmimodem
+	endif
+
 	LOCAL_SHARED_LIBRARIES := libglib libdbus libdl liblog
 
 	LOCAL_C_INCLUDES += external/glib external/glib/glib external/dbus
@@ -76,6 +91,8 @@ include $(CLEAR_VARS)
 		-Wl,--wrap=vsyslog -Wl,--wrap=syslog \
 
 include $(BUILD_EXECUTABLE)
+
+## XXX how nasty are these hacks of making .c files be build targets?
 
 ## symlink .h files so the .c files can #include <ofono/X.h> . This is done in Makefile.am
 # make -f (printf 'printvars:\n\t @echo $(BUILT_SOURCES)' | psub) -f Makefile
@@ -104,3 +121,8 @@ $(addprefix $(LOCAL_PATH)/, $(LOCAL_SRC_FILES)): $(addprefix $(LOCAL_PATH)/, \
 $(LOCAL_PATH)/include/ofono/%.h: $(LOCAL_PATH)/include/%.h
 	$(hide) mkdir -p $(shell dirname $@)
 	$(hide) ln -s $(realpath $<) $@
+
+$(LOCAL_PATH)/src/plugin.c: $(LOCAL_PATH)/src/builtin.h
+
+$(LOCAL_PATH)/src/builtin.h: $(LOCAL_PATH)/src/genbuiltin
+	$(hide) $< $(OFONO_PLUGINS) > $@
